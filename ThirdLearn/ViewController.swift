1ff999af1e6b9cec0bb4f6d1b694d297c45e70c7
ouxuesen <ouxuesen@ginkgofit.com>
2019-10-14 15:16:21 +0800

    init

---
+++ ThirdLearn/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ThirdLearn
+//
+//  Created by ou xuesen on 2019/10/14.
+//  Copyright © 2019 ou xuesen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
